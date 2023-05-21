22218c0ffa6a82f8bb19eccd4c0ff08c04cf4ed8
giooogk <macedo1999.gk@gmail.com>
2023-05-21 16:40:59 -0300

    fix: ajustar operações com valores negativos

--- account/views.py
+++ account/views.py
@@ -34,6 +34,8 @@ def credit(request):
         number = request.POST.get('number')
         value = Decimal(request.POST.get('value'))
         account = get_object_or_404(Account, number=number)
+        if value < 0:
+            return JsonResponse({'error': 'Valor inválido'}, status=400)
         account.balance += value
         account.save()
         return HttpResponse(f'Crédito realizado na conta {account.number}. Novo saldo: {account.balance}')
@@ -44,6 +46,8 @@ def debit(request):
         number = request.POST.get('number')
         value = Decimal(request.POST.get('value'))
         account = get_object_or_404(Account, number=number)
+        if value < 0:
+            return JsonResponse({'error': 'Valor inválido'}, status=400)
         if account.balance >= value:
             account.balance -= value
             account.save()
@@ -59,10 +63,14 @@ def transfer(request):
         value = Decimal(request.POST.get('value'))
         source_account = get_object_or_404(Account, number=source_number)
         destination_account = get_object_or_404(Account, number=destination_number)
-        
+
+        if value < 0:
+            return JsonResponse({'error': 'Valor inválido'}, status=400)
         if source_account.balance < value:
             return JsonResponse({'error': 'Conta de origem não possui saldo insuficiente'}, status=400)  
         source_account.balance -= value
         destination_account.balance += value
-
+        source_account.save()
+        destination_account.save()
+        
     return render(request, 'account/transfer.html')
